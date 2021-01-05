d41c0afb55c86f2e4e676f861802fe6c56e3c3a5
Davis Ward <dzw0042@auburn.edu>
2021-01-04 20:43:54 -0600

    fixed register form

--- routes/users.js
+++ routes/users.js
@@ -4,13 +4,13 @@ var router = express.Router();
 
 // Login Page
 router.get('/login', function(req, res, next) {
-    res.send('Login');
+    res.render('pages/login')
 });
 
 
 // Register Page
 router.get('/register', function(req, res, next) {
-
+    res.render('pages/register');
 });
 
 router.post('/register', function(req, res, next) {
