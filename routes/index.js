163f7860dd8d3abe20645572ecb216fca86e3c9e
Davis Ward <dzw0042@auburn.edu>
2021-01-07 01:02:48 -0600

    fixed forward authentication and polished login, register, and navbar

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const { ensureAuthenticated } = require('../config/auth');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', ensureAuthenticated, function(req, res, next) {
   res.render('pages/index', { firstname: req.user.firstname });
 });
 
--- routes/users.js
+++ routes/users.js
@@ -1,97 +1,131 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const db = require('../models');
+const db = require("../models");
 const User = db.User;
 const Op = db.Sequelize.Op;
 
-const bcrypt = require('bcryptjs');
-const passport = require('passport');
+const bcrypt = require("bcryptjs");
+const passport = require("passport");
 
+const { ensureAuthenticated } = require("../config/auth");
 
 // Login Page
-router.get('/login', function(req, res, next) {
-    res.render('pages/login')
+router.get("/login", function (req, res, next) {
+  res.render("pages/login");
 });
 
-router.post('/login', function(req, res, next) {
-    passport.authenticate('local', {
-        successRedirect: '/',
-        failureRedirect: '/users/login',
-        failureFlash: true
-    })(req, res, next);
+router.post("/login", function (req, res, next) {
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/users/login",
+    failureFlash: true,
+  })(req, res, next);
 });
-
 
 // Register Page
-router.get('/register', function(req, res, next) {
-    res.render('pages/register');
+router.get("/register", function (req, res, next) {
+  res.render("pages/register");
 });
 
-router.post('/register', function(req, res, next) {
-    //console.log(req.body);
-    const { firstname, lastname, email, password, usertype} = req.body;
+router.post("/register", function (req, res, next) {
+  //console.log(req.body);
+  const { firstname, lastname, email, password, usertype } = req.body;
 
-    // simple validation checks
-    let errors = [];
-    // check required fields
-    if (!firstname || !lastname || !email || !password || !usertype) {
-        errors.push({msg: 'Please fill in all fields'});
-    }
+  // simple validation checks
+  let errors = [];
+  // check required fields
+  if (!firstname || !lastname || !email || !password || !usertype) {
+    errors.push({ msg: "Please fill in all fields" });
+  }
 
-    // check password length
-    if (password.length < 6) {
-        errors.push({msg: 'Password should be at least 6 characters'});
-    }
+  // check password length
+  if (password.length < 6) {
+    errors.push({ msg: "Password should be at least 6 characters" });
+  }
 
-    if (errors.length > 0) {
-        res.render('pages/register', {errors, firstname, lastname, email, usertype});
-    } else {
-        // validation has passed
-        console.log('validation has passed.');
+  if (errors.length > 0) {
+    res.render("pages/register", {
+      errors,
+      firstname,
+      lastname,
+      email,
+      usertype,
+    });
+  } else {
+    // validation has passed
+    console.log("validation has passed.");
 
-        User.findOne({
-            where: {email: email}
-        }).then(function(user) {
-            console.log(user);
+    User.findOne({
+      where: { email: email },
+    }).then(function (user) {
+      console.log(user);
 
-            // save the new user to the database if they don't already exist
-            if (!user) {
+      // save the new user to the database if they don't already exist
+      if (!user) {
+        // create a new user
+        const newUser = {
+          firstname: firstname,
+          lastname: lastname,
+          email: email,
+          password: password,
+          usertype: usertype,
+        };
 
-            // create a new user
-            const newUser = {
-                firstname: firstname,
-                lastname: lastname,
-                email: email,
-                password: password,
-                usertype: usertype
-            }
+        // hash the password
+        bcrypt.genSalt(10, (err, salt) =>
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
 
-            // hash the password
-            bcrypt.genSalt(10 , (err, salt) => bcrypt.hash(newUser.password, salt, (err, hash) => {
-                if(err) throw err;
+            newUser.password = hash;
+            console.log(newUser);
 
-                newUser.password = hash;
-                console.log(newUser);
-                
-                // add the new user to the database
-                User.create(newUser).then(function() {
-                    console.log('New User was added to the database.');
-                    req.flash('success_msg', 'You are now registered. Please log into your account!');
-                    res.redirect('/users/login');
-                }).catch(function(err) {
-                    console.log(err, 'Something went wrong when adding new User to database');
-                });
-            }));
-            } else {
-            console.log('User with same email already exist');
-            errors.push({msg: 'Email is already registered'});
-            res.render('pages/register', {errors, firstname, lastname, email, usertype});
-            }
+            // add the new user to the database
+            User.create(newUser)
+              .then(function () {
+                console.log("New User was added to the database.");
+                req.flash(
+                  "success_msg",
+                  "You are now registered. Please log into your account!"
+                );
+                res.redirect("/users/login");
+              })
+              .catch(function (err) {
+                console.log(
+                  err,
+                  "Something went wrong when adding new User to database"
+                );
+              });
+          })
+        );
+      } else {
+        console.log("User with same email already exist");
+        errors.push({ msg: "Email is already registered" });
+        res.render("pages/register", {
+          errors,
+          firstname,
+          lastname,
+          email,
+          usertype,
         });
-    }
+      }
+    });
+  }
+});
 
+router.get("/logout", ensureAuthenticated, function (req, res, next) {
+  req.logout();
+  req.flash("success_msg", "You have successfully logged out.");
+  res.redirect("/users/login");
+});
 
+router.get("/account", ensureAuthenticated, function (req, res, next) {
+  res.render("pages/account", {
+    firstname: req.user.firstname,
+    lastname: req.user.lastname,
+    email: req.user.email,
+    usertype: req.user.usertype,
+  });
 });
 
 module.exports = router;
